592699c3bce747057d459c833c7f5b1fbc2cff4c
Nasrul <98256020+naaasruul@users.noreply.github.com>
2022-04-26 16:23:38 +0800

    Create string.js

---
+++ string.js
@@ -0,0 +1,23 @@
+var sentence = "The Moose Academy";
+console.log(sentence.length)
+// 17
+console.log(sentence.toUpperCase());
+// "THE MOOSE ACADEMY"
+console.log(sentence.toLowerCase());
+// "the moose academy"
+console.log(sentence.charAt(2));
+//T
+console.log(sentence.indexOf());
+// -1
+console.log(sentence.lastIndexOf());
+// -1
+console.log(sentence.substring(10));
+// Academy
+//The Moose Academy
+console.log(sentence.split());
+//["The Moose Academy"]
+console.log(sentence.replace());
+//The Moose Academy
+console.log(sentence.split(2));
+console.log(sentence.lastIndexOf());
+console.log(sentence.split(" "))
